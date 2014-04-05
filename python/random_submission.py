539d2573a30c5071c4d237c66b3c9df819486522
Emanuele Olivetti <olivetti@fbk.eu>
2014-04-05 16:49:51 +0200

    added code for simple random submission

---
+++ python/random_submission.py
@@ -0,0 +1,23 @@
+import numpy as np
+from scipy.io import loadmat
+
+
+
+if __name__ == '__main__':
+
+    np.random.seed(0)
+    subjects = range(17, 24)
+
+    f = open('random_submission.csv','w')
+    print >> f, 'Id,Prediction'
+    for subject in subjects:
+        filename = 'test/test_subject'+str(subject)+'.mat'
+        print "Loading", filename
+        data = loadmat(filename, squeeze_me=True)
+        ids = data['Id']
+        prediction = (np.random.rand(len(ids)) > 0.5).astype(np.int)
+        for i, ids_i in enumerate(ids):
+            print >> f, str(ids_i) + ',' + str(prediction[i])
+
+    f.close()
+                      
