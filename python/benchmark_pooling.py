e807770a53091082463f77da71a14c38e5d2e800
Emanuele Olivetti <olivetti@fbk.eu>
2014-04-05 19:57:19 +0200

    added benchmark with simple pooling and linear classifier

---
+++ python/benchmark_pooling.py
@@ -0,0 +1,106 @@
+"""Simple prediction of the test set by pooling all the trials of all
+subjects in one dataset, extracting the MEG data in the first 500ms
+and using a linear classifier.
+"""
+
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from scipy.io import loadmat
+
+
+def create_features(XX, tmin, tmax, sfreq):
+    print "Applying the desired time window."
+    beginning = np.round((tmin - tmin_original) * sfreq).astype(np.int)
+    end = np.round((tmax - tmin_original) * sfreq).astype(np.int)
+    XX = XX[:, :, beginning:end].copy()
+
+    print "2D Reshaping."
+    XX = XX.reshape(XX.shape[0], -1)
+
+    print "Feature-wise Normalization."
+    XX -= XX.mean(0)
+    XX = np.nan_to_num(XX / XX.std(0))
+
+    return XX
+
+
+if __name__ == '__main__':
+
+    subjects_train = range(1, 17)
+    subjects_test = range(17, 24)
+
+    tmin = 0.0
+    tmax = 0.500
+
+    X_train = []
+    y_train = []
+    X_test = []
+    ids_test = []
+
+    print "Creating the trainset."
+    for subject in subjects_train:
+        filename = 'data/train_subject%02d.mat' % subject
+        print "Loading", filename
+        data = loadmat(filename, squeeze_me=True)
+        XX = data['X']
+        yy = data['y']
+        sfreq = data['sfreq']
+        tmin_original = data['tmin']
+        print "Dataset summary:"
+        print "XX:", XX.shape
+        print "yy:", yy.shape
+        print "sfreq:", sfreq
+
+        XX = create_features(XX, tmin, tmax, sfreq)
+
+        X_train.append(XX)
+        y_train.append(yy)
+
+    X_train = np.vstack(X_train)
+    y_train = np.concatenate(y_train)
+    print "Trainset:", X_train.shape
+
+    print
+    print "Creating the testset."
+    for subject in subjects_test:
+        filename = 'data/test_subject%02d.mat' % subject
+        print "Loading", filename
+        data = loadmat(filename, squeeze_me=True)
+        XX = data['X']
+        ids = data['Id']
+        sfreq = data['sfreq']
+        tmin_original = data['tmin']
+        print "Dataset summary:"
+        print "XX:", XX.shape
+        print "ids:", ids.shape
+        print "sfreq:", sfreq
+
+        XX = create_features(XX, tmin, tmax, sfreq)
+
+        X_test.append(XX)
+        ids_test.append(ids)
+
+    X_test = np.vstack(X_test)
+    ids_test = np.concatenate(ids_test)
+    print "Testset:", X_test.shape
+
+    print
+    clf = LogisticRegression(random_state=0) # beware, you need 10Gb to train from 16 subjects!
+    print "Classifier:"
+    print clf
+    print "Training."
+    clf.fit(X_train, y_train)
+    print "Predicting."
+    y_pred = clf.predict(X_test)
+
+    print
+    filename_submission = "submission.csv"
+    print "Creating submission file", filename_submission
+    f = open(filename_submission, "w")
+    print >> f, "Id,Prediction"
+    for i in range(len(y_pred)):
+        print >> f, str(ids_test[i]) + "," + str(y_pred[i])
+
+    f.close()
+    print "Done."
+    
